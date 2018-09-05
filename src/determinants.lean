69761e2d87f48cbd96d3907f704e81dcfffd405d
Johan Commelin <johan@commelin.net>
2018-09-05 12:00:15 +0200

    SL n R is a kernel

--- src/determinants.lean
+++ src/determinants.lean
@@ -1,50 +1,57 @@
+import tactic.tidy
 import group_theory.subgroup
 import .matrices
 
 universes u v
 
-section
+class is_monoid_hom {α : Type u} {β : Type v} [monoid α] [monoid β] (f : α → β) : Prop :=
+(map_one : f 1 = 1)
+(map_mul : ∀ {x y}, f (x * y) = f x * f y)
+
+definition units.map {α : Type u} {β : Type v} [monoid α] [monoid β] (f : α → β) [is_monoid_hom f] :
+units α → units β :=
+λ u, ⟨f u.val, f u.inv, sorry, sorry⟩
+
+instance units.map_is_group_hom {α : Type u} {β : Type v} [monoid α] [monoid β] (f : α → β) [is_monoid_hom f] :
+is_group_hom (units.map f) :=
+⟨λ a b, begin
+ext,
+exact is_monoid_hom.map_mul f,
+end⟩
+
+namespace matrix
 variables {R : Type u} [ring R]
 
-def matrix.det {n : ℕ} : matrix R n n → R := sorry
+def det {n : ℕ} : matrix R n n → R := sorry
 
-open matrix
+lemma det_zero {n : ℕ} : det (0 : matrix R n n) = 0 := sorry
 
-class is_monoid_hom {α : Type u} {β : Type v} [monoid α] [monoid β] (f : α → β) : Prop :=
-(map_mul : ∀ (x y), f (x * y) = f x * f y)
-(map_one : f 1 = 1)
+lemma det_one {n : ℕ} : det (1 : matrix R n n) = 1 := sorry
 
-instance det_homomorphic (n : ℕ) : is_monoid_hom (@det R _ n) := sorry
-end
+lemma det_mul {n : ℕ} (M : matrix R n n) (N : matrix R n n) : det (M * N) = det M * det N := sorry
 
-section
-open matrix
-variables (n : ℕ) (R : Type u) [ring R]
+instance det_homomorphic (n : ℕ) : is_monoid_hom (det : matrix R n n → R) :=
+{ map_one := det_one,
+  map_mul := det_mul }
 
-def GL  := units (matrix R n n)
+end matrix
 
-instance GL_group : group (GL n R) := by unfold GL; apply_instance
+def GL (n : ℕ) (R : Type u) [ring R] := units (matrix R n n)
 
-def SL := {M : GL n R | det M.val = (1 : R)}
+namespace GL
 
-instance SL_subgroup : is_subgroup (SL n R) :=
-begin
-  refine_struct {..},
-  { exact is_monoid_hom.map_one det },
-  { intros a b a_in b_in,
-    have := is_monoid_hom.map_mul det a.val b.val,
-    dsimp [SL] at *,
-    rw [a_in, b_in] at this,
-    simpa using this },
-  { intros a a_in,
-    have := is_monoid_hom.map_mul det a.val a⁻¹.val,
-    dsimp [SL] at *,
-    rw [a_in] at this,
-    simp at this,
-    rw ←this,
-    sorry}
-end
+variables {n : ℕ} {R : Type u} [ring R]
 
-instance SL_group (n : ℕ) (R : Type u) [ring R] : group (SL n R) := 
-by apply_instance
-end
+instance : group (GL n R) := by unfold GL; apply_instance
+
+def det : GL n R → units R := units.map matrix.det
+
+instance : is_group_hom (det : GL n R → units R) := units.map_is_group_hom matrix.det
+
+lemma det_one : det (1 : GL n R) = 1 := is_group_hom.one det
+
+lemma det_mul (M : GL n R) (N : GL n R) : det (M * N) = det M * det N := is_group_hom.mul det M N
+
+end GL
+
+def SL (n : ℕ) (R : Type u) [ring R] := is_group_hom.ker (GL.det : GL n R → units R)
