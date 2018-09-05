9da95828672a2dad600c88068ab31e68b51d7b1c
Sean Leather <sean.leather@gmail.com>
2018-09-05 09:24:30 +0200

    Swap matrix parameter order

--- src/matrices.lean
+++ src/matrices.lean
@@ -1,35 +1,35 @@
 import algebra.big_operators data.set.finite
 
-def matrix (α : Type*) (m n : ℕ) := fin m → fin n → α
+def matrix (m n : ℕ) (α : Type*) := fin m → fin n → α
 
 namespace matrix
-variables {α : Type*} [ring α]
 variables {l m n o : ℕ}
+variables {α : Type*} [ring α]
 
-instance : has_zero (matrix α m n) :=
+instance : has_zero (matrix m n α) :=
 ⟨λ _ _, 0⟩
 
-instance : has_neg (matrix α m n) :=
+instance : has_neg (matrix m n α) :=
 ⟨λ M x y, - M x y⟩
 
-instance : has_add (matrix α m n) :=
+instance : has_add (matrix m n α) :=
 ⟨λ M N x y, M x y + N x y⟩
 
-@[simp] theorem add_val {M N : matrix α m n} {x : fin m} {y : fin n} : (M + N) x y = M x y + N x y :=
+@[simp] theorem add_val {M N : matrix m n α} {x : fin m} {y : fin n} : (M + N) x y = M x y + N x y :=
 rfl
 
-theorem add_assoc (L : matrix α m n) (M : matrix α m n) (N : matrix α m n) :
+theorem add_assoc (L : matrix m n α) (M : matrix m n α) (N : matrix m n α) :
   L + (M + N) = (L + M) + N :=
 funext $ λ x, funext $ λ y, by simp
 
-def mul (M : matrix α l m) (N : matrix α m n) : matrix α l n :=
+def mul (M : matrix l m α) (N : matrix m n α) : matrix l n α :=
 λ x z, finset.univ.sum (λ y, M x y * N y z)
 
-@[simp] theorem mul_val {M : matrix α l m} {N : matrix α m n} {x : fin l} {z : fin n} :
+@[simp] theorem mul_val {M : matrix l m α} {N : matrix m n α} {x : fin l} {z : fin n} :
   (M.mul N) x z = finset.univ.sum (λ y, M x y * N y z) :=
 rfl
 
-theorem mul_assoc (L : matrix α l m) (M : matrix α m n) (N : matrix α n o) :
+theorem mul_assoc (L : matrix l m α) (M : matrix m n α) (N : matrix n o α) :
   L.mul (M.mul N) = (L.mul M).mul N :=
 funext $ λ x, funext $ λ z,
   calc finset.univ.sum (λ (y₁ : fin m), L x y₁ * finset.univ.sum (λ (y₂ : fin n), M y₁ y₂ * N y₂ z))
@@ -40,6 +40,6 @@ funext $ λ x, funext $ λ z,
     ... = finset.univ.sum (λ (y₂ : fin n), finset.univ.sum (λ (y₁ : fin m), L x y₁ * M y₁ y₂) * N y₂ z) :
       by congr; funext; rw ←finset.sum_mul
 
-instance (n : ℕ) : ring (matrix α n n) := sorry
+instance (n : ℕ) : ring (matrix n n α) := sorry
 
 end matrix
