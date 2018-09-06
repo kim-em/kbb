0fd02e4413b39ea4e4681543344a4dd373975a7d
Johan Commelin <johan@commelin.net>
2018-09-06 11:37:12 +0200

    Whoops forgot to save. Stupid VScode.

--- src/determinants.lean
+++ src/determinants.lean
@@ -37,23 +37,3 @@ instance : is_monoid_hom (det : matrix n n R → R) :=
   map_mul := det_mul }
 
 end matrix
-
-def GL (n : ℕ) (R : Type u) [ring R] := units (matrix (fin n) (fin n) R)
-
-namespace GL
-
-variables {n : ℕ} {R : Type u} [ring R]
-
-instance : group (GL n R) := by unfold GL; apply_instance
-
-def det : GL n R → units R := units.map matrix.det
-
-instance : is_group_hom (det : GL n R → units R) := units.map_is_group_hom matrix.det
-
-@[simp] lemma det_one : det (1 : GL n R) = 1 := is_group_hom.one det
-
-@[simp] lemma det_mul (M : GL n R) (N : GL n R) : det (M * N) = det M * det N := is_group_hom.mul det M N
-
-end GL
-
-def SL (n : ℕ) (R : Type u) [ring R] := is_group_hom.ker (GL.det : GL n R → units R)
