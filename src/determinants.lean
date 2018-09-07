93de9b17ec384ae5edaaf56e03036c1444a3ae57
Johan M. Commelin <johan@commelin.net>
2018-09-07 23:40:33 +0200

    Prove det_zero and det_one

--- src/determinants.lean
+++ src/determinants.lean
@@ -10,10 +10,9 @@ universes u v
 namespace matrix
 variables {n : ℕ} {R : Type v} [comm_ring R]
 
-
 instance {n : ℕ} : group (equiv.perm (fin n)) := by apply_instance
 
-noncomputable instance bij_fintype {n : ℕ} : fintype {f : fin n → fin n // function.bijective f} := 
+instance bij_fintype {n : ℕ} : fintype {f : fin n → fin n // function.bijective f} := 
 set_fintype _
 
 noncomputable instance equiv_perm_fin_finite {n:ℕ} : fintype (equiv.perm(fin n)):=
@@ -43,7 +42,7 @@ begin
   have H : ∀ σ : equiv.perm (fin n.succ), e σ * (0 : R) ^ (finset.univ : finset (fin n.succ)).card = 0,
   begin
     intro x,
-    suffices :  (0 : R) ^ (finset.univ : finset (fin n.succ)).card = 0,
+    suffices : (0 : R) ^ (finset.univ : finset (fin n.succ)).card = 0,
     rw this, simp,
     suffices : (finset.univ : finset (fin n.succ)).card = n.succ,
     rw [this, pow_succ], simp,
@@ -56,19 +55,66 @@ begin
   simp
 end
 
+#print equiv.perm.sign.is_group_hom
+
 @[simp] lemma det_one : det (1 : matrix (fin n) (fin n) R) = (1 : R) :=
 begin
   simp [det],
   have H : ∀ σ : equiv.perm (fin n),
-    finset.univ.prod (λ (i : fin n), (1 : matrix _ _ R) (σ.1 i) i) = finset.univ.prod (λ (i : fin n), ite ((σ.to_fun i) = i) 1 0),
+    finset.univ.prod (λ (i : fin n), (1 : matrix _ _ R) (σ.1 i) i) =
+    ite (σ = 1) 1 0,
   begin
-    sorry
+    intro σ,
+    split_ifs,
+    { conv {
+        to_lhs, congr, skip, funext,
+        rw [h, one_val],
+      },
+      conv {
+        to_rhs,
+        rw ← @finset.prod_const_one (fin n) R finset.univ _
+      },
+      apply finset.prod_congr rfl,
+      intros,
+      split_ifs,
+      refl,
+      change ((1 : equiv.perm (fin n)) x ≠ x) at h_1,
+      have : ((1 : equiv.perm (fin n)) x = x) := by simp,
+      exfalso, exact h_1 this
+    },
+    { have : ∃ i : fin n, σ.to_fun i ≠ i :=
+      begin
+        by_contra,
+        simp at a,
+        suffices : σ = 1,
+        exact h this,
+        apply equiv.ext,
+        simp [a],
+        exact a
+      end,
+      cases this with i hi,
+      rw ← @finset.prod_sdiff,
+      work_on_goal 2 {
+        exact singleton i
+      },
+      tidy,
+      suffices : (1 : matrix _ _ R) (σ.to_fun i) i = 0,
+      rw this, simp,
+      simp [hi] }
   end,
   conv {
     to_lhs, congr, skip, funext,
     rw H,
   },
-  sorry
+  transitivity finset.sum (finset.singleton 1 : finset (equiv.perm (fin n))) _,
+  { symmetry,
+    apply finset.sum_subset,
+    { intros i H, simp },
+    { intros, simp at a, split_ifs, simp } },
+  simp [e],
+  rw is_group_hom.one equiv.perm.sign,
+  simp,
+  apply_instance
 end
 
 @[simp] lemma det_mul (M : matrix (fin n) (fin n) R) (N : matrix (fin n) (fin n) R) :
@@ -82,41 +128,41 @@ instance : is_monoid_hom (det : matrix (fin n) (fin n) R → R) :=
 
 
 
-def cross_out_column
-  (A : matrix (fin (n+1)) (fin (n+1)) R) (m : fin (n+1)) : matrix (fin n) (fin n) R := 
-λ i j,
-if j.1 < m.1 then  A i.succ j.raise else 
-  A i.succ j.succ
+-- def cross_out_column
+--   (A : matrix (fin (n+1)) (fin (n+1)) R) (m : fin (n+1)) : matrix (fin n) (fin n) R := 
+-- λ i j,
+-- if j.1 < m.1 then  A i.succ j.raise else 
+--   A i.succ j.succ
 
-def det_laplace : Π {n: nat}, matrix (fin (n+1)) (fin (n+1)) R → R
-| 0 := λ A, A 0 0
-| (n + 1) := λ A, 
-  finset.sum finset.univ (λ k : fin (n+2), (-1 : R)^(k.1) * (A 0 k) *
- det_laplace (cross_out_column A k))
+-- def det_laplace : Π {n: nat}, matrix (fin (n+1)) (fin (n+1)) R → R
+-- | 0 := λ A, A 0 0
+-- | (n + 1) := λ A, 
+--   finset.sum finset.univ (λ k : fin (n+2), (-1 : R)^(k.1) * (A 0 k) *
+--  det_laplace (cross_out_column A k))
 
-namespace det_laplace
+-- namespace det_laplace
 
-lemma det_1_1_zero : det_laplace (0 : matrix (fin 1) (fin 1) R) = 0 := rfl
+-- lemma det_1_1_zero : det_laplace (0 : matrix (fin 1) (fin 1) R) = 0 := rfl
 
-lemma det_1_1_one : det_laplace (1 : matrix (fin 1) (fin 1) R) = 1 := rfl
+-- lemma det_1_1_one : det_laplace (1 : matrix (fin 1) (fin 1) R) = 1 := rfl
 
-lemma cross_out_column_zero {m : fin (n+1)} : cross_out_column (0 : matrix _ _ R) m = 0 :=
-by funext; unfold cross_out_column; split_ifs; simp
+-- lemma cross_out_column_zero {m : fin (n+1)} : cross_out_column (0 : matrix _ _ R) m = 0 :=
+-- by funext; unfold cross_out_column; split_ifs; simp
 
-lemma cross_out_column_one_0 : cross_out_column (1 : matrix (fin (n+1)) _ R) 0 = 1 :=
-begin
-  funext,
-  unfold cross_out_column,
-  split_ifs;
-  simp [one_val];
-  split_ifs;
-  tidy;
-  exfalso;
-  try { exact nat.not_lt_zero _ h, },
-  replace h_1 := fin.veq_of_eq h_1,
-  replace h_2 := fin.vne_of_ne h_2,
-  tidy
-end
+-- lemma cross_out_column_one_0 : cross_out_column (1 : matrix (fin (n+1)) _ R) 0 = 1 :=
+-- begin
+--   funext,
+--   unfold cross_out_column,
+--   split_ifs;
+--   simp [one_val];
+--   split_ifs;
+--   tidy;
+--   exfalso;
+--   try { exact nat.not_lt_zero _ h, },
+--   replace h_1 := fin.veq_of_eq h_1,
+--   replace h_2 := fin.vne_of_ne h_2,
+--   tidy
+-- end
 
 -- lemma cross_out_column_one_ne0 (m : fin (n+1)) (h : m ≠ 0) : det_laplace (cross_out_column (1 : matrix _ _ R) m) = 0 :=
 -- begin
@@ -133,42 +179,42 @@ end
 --   tidy
 -- end
 
-@[simp] lemma det_zero : det_laplace (0 : matrix (fin (n+1)) (fin (n+1)) R) = (0 : R) :=
-begin
-  induction n with n ih,
-  exact det_1_1_zero,
-  unfold det_laplace,
-  conv {
-    to_lhs,
-    congr, skip,
-    funext,
-    rw [cross_out_column_zero, ih] },
-  simp
-end
+-- @[simp] lemma det_zero : det_laplace (0 : matrix (fin (n+1)) (fin (n+1)) R) = (0 : R) :=
+-- begin
+--   induction n with n ih,
+--   exact det_1_1_zero,
+--   unfold det_laplace,
+--   conv {
+--     to_lhs,
+--     congr, skip,
+--     funext,
+--     rw [cross_out_column_zero, ih] },
+--   simp
+-- end
 
-@[simp] lemma det_one : det_laplace (1 : matrix (fin (n+1)) (fin (n+1)) R) = (1 : R) :=
-begin
-  induction n with n ih,
-  exact det_1_1_one,
-  unfold det_laplace,
-  have H : ∀ k : fin (n + 2), (-1) ^ k.val * ite (0 = k) (1 : R) 0 * det_laplace (cross_out_column 1 k) = (-1) ^ k.val * ite (0 = k) 1 0,
-  begin
-    intro k, split_ifs,
-    { rw [← h, cross_out_column_one_0, ih], simp },
-    { simp }
-  end,
-  conv {
-    to_lhs,
-    congr, skip,
-    funext,
-    rw [one_val, H k] },
-    transitivity finset.sum (finset.singleton 0 : finset (fin (n+2))) _,
-    { symmetry,
-      apply finset.sum_subset,
-      { intros i H, simp },
-      { intros, simp at a, simp [ne.symm a] } },
-    simp, refl
-end
+-- @[simp] lemma det_one : det_laplace (1 : matrix (fin (n+1)) (fin (n+1)) R) = (1 : R) :=
+-- begin
+--   induction n with n ih,
+--   exact det_1_1_one,
+--   unfold det_laplace,
+--   have H : ∀ k : fin (n + 2), (-1) ^ k.val * ite (0 = k) (1 : R) 0 * det_laplace (cross_out_column 1 k) = (-1) ^ k.val * ite (0 = k) 1 0,
+--   begin
+--     intro k, split_ifs,
+--     { rw [← h, cross_out_column_one_0, ih], simp },
+--     { simp }
+--   end,
+--   conv {
+--     to_lhs,
+--     congr, skip,
+--     funext,
+--     rw [one_val, H k] },
+--     transitivity finset.sum (finset.singleton 0 : finset (fin (n+2))) _,
+--     { symmetry,
+--       apply finset.sum_subset,
+--       { intros i H, simp },
+--       { intros, simp at a, simp [ne.symm a] } },
+--     simp, refl
+-- end
 
 -- @[simp] lemma det_mul (M : matrix (fin (n + 1)) _ R) (N : matrix (fin (n + 1)) _ R) :
 -- det_laplace (M * N) = det_laplace M * det_laplace N :=
@@ -191,6 +237,6 @@ end
 --   sorry
 -- end
 
-end det_laplace
+-- end det_laplace
 
 end matrix
