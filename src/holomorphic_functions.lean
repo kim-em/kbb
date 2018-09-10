92a43664d64216fd42e2a8cbdea39a5c43d63d9b
Johan Commelin <johan@commelin.net>
2018-09-10 12:08:37 +0200

    Sum and neg of holomorphic are holomorphic

--- src/holomorphic_functions.lean
+++ src/holomorphic_functions.lean
@@ -20,15 +20,23 @@ let error_term (h : ℂ) : ℝ :=
 
 section
 variables  {α : Type*} {β : Type*} {s : set α}
+
 def extend_by_zero [has_zero β] (f : s → β) : α → β :=
 λ z, if h : z ∈ s then f ⟨z, h⟩ else 0
-
 
 lemma extend_by_zero_add [add_group β] (f g : s → β) :
 extend_by_zero (f + g) = extend_by_zero f + extend_by_zero g :=
 by ext z ; by_cases h : z ∈ s ; simp [extend_by_zero, h]
-end
 
+lemma extend_by_zero_mul [semiring β] (f g : s → β) :
+extend_by_zero (f * g) = extend_by_zero f * extend_by_zero g :=
+by ext z ; by_cases h : z ∈ s ; simp [extend_by_zero, h]
+
+lemma extend_by_zero_neg [add_group β] (f : s → β) :
+extend_by_zero (-f) = -extend_by_zero f :=
+by ext z ; by_cases h : z ∈ s ; simp [extend_by_zero, h]
+
+end
 
 /-- holomorphic function from a subset of ℂ. Correct definition if domain is open. -/
 def is_holomorphic {domain : set ℂ} (f : domain → ℂ) :=
@@ -44,6 +52,15 @@ instance : is_subring {f : domain → ℂ | is_holomorphic f} :=
   one_mem  := sorry,
 }-/
 
+lemma const_hol (domain_open : is_open domain) (c : ℂ) : is_holomorphic (λ z : domain, (c : ℂ)) :=
+begin
+  intro z₀,
+  existsi (0 : ℂ),
+  dsimp [has_complex_derivative_at],
+  simp,
+  sorry
+end
+
 lemma zero_hol : is_holomorphic (λ z : domain, (0 : ℂ)) :=
 begin
   intro z₀,
@@ -54,6 +71,40 @@ begin
     dsimp only [extend_by_zero],
     by_cases h : z ∈ domain ; simp [h] ; refl },
   simp [this, tendsto_const_nhds]
+end
+
+lemma one_hol (domain_open : is_open domain) : is_holomorphic (λ z : domain, (1 : ℂ)) := const_hol domain_open 1
+
+lemma add_hol (f g : domain → ℂ) (f_hol : is_holomorphic f) (g_hol : is_holomorphic g) : is_holomorphic (f + g) :=
+begin
+  intro z₀,
+  cases f_hol z₀ with f'z₀ Hf,
+  cases g_hol z₀ with g'z₀ Hg,
+  existsi (f'z₀ + g'z₀),
+  rw extend_by_zero_add,
+  have Hfg : tendsto (λ (h : ℂ), abs ((extend_by_zero f (↑z₀ + h) - (extend_by_zero f ↑z₀ + f'z₀ * h)) / h) +
+         abs ((extend_by_zero g (↑z₀ + h) - (extend_by_zero g ↑z₀ + g'z₀ * h)) / h)) (nhds 0) (nhds 0) :=
+  by simpa using tendsto_add Hf Hg,
+  refine squeeze_zero _ _ Hfg,
+  { intro h, apply complex.abs_nonneg },
+  { intro h,
+    refine le_trans (le_of_eq _) (complex.abs_add _ _),
+    congr,
+    dsimp,
+    ring }
+end
+
+lemma neg_hol (f : domain → ℂ) (f_hol : is_holomorphic f) : is_holomorphic (-f) :=
+begin
+  intro z₀,
+  cases f_hol z₀ with f'z₀ H,
+  existsi -f'z₀,
+  rw extend_by_zero_neg,
+  suffices : tendsto (λ (h : ℂ), abs ((-(extend_by_zero f (↑z₀ + h) + -(extend_by_zero f ↑z₀ + f'z₀ * h))) / h))
+    (nhds 0) (nhds 0),
+  { simpa [has_complex_derivative_at] },
+  conv { congr, funext, rw [neg_div, complex.abs_neg], },
+  exact H
 end
 
 -- instance complex_fun_module : module ℂ (domain → ℂ) := sorry
