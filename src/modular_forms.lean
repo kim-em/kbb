4f62193222f64b2275bfb0fef0273d5b834b9c12
Patrick Massot <patrickmassot@free.fr>
2018-09-10 21:53:19 +0200

    Experiment shorter proofs using mono

--- src/modular_forms.lean
+++ src/modular_forms.lean
@@ -1,4 +1,5 @@
 import tactic.ring
+import tactic.monotonicity.interactive
 import group_theory.group_action
 import algebra.module
 import algebra.pi_instances
@@ -9,6 +10,8 @@ import .upper_half_space
 universes u v
 
 open complex
+
+local attribute [monotonic] complex.abs_add
 
 local notation `ℍ` := upper_half_space
 
@@ -22,7 +25,7 @@ instance (k : ℕ) : is_submodule (is_Petersson_weight_ k) :=
 { zero_ := λ M z, by simp,
   add_  := λ f g hf hg M z, begin
     suffices : f (SL2Z_H M z) + g (SL2Z_H M z) = (↑(M.c) * ↑z + ↑(M.d)) ^ k * (f z + g z),
-      by simp at *; assumption,
+      by simp at * ; assumption,
     rw [hf, hg],
     ring
   end,
@@ -30,50 +33,41 @@ instance (k : ℕ) : is_submodule (is_Petersson_weight_ k) :=
     suffices : c * f (SL2Z_H M z) = (↑(M.c) * ↑z + ↑(M.d)) ^ k * (c * f z),
       by simp at *; assumption,
     rw hyp M z,
-    ring 
+    ring
   end }
 
 instance : is_submodule is_bound_at_infinity :=
 { zero_ := ⟨0, ⟨0, λ z h, by simp⟩⟩,
   add_  := λ f g hf hg, begin
-    cases hf with Mf hMf,
-    cases hg with Mg hMg,
-    cases hMf with Af hAMf,
-    cases hMg with Ag hAMg,
-    existsi (Mf + Mg),
-    existsi (max Af Ag),
+    rcases hf with ⟨Mf, ⟨Af, hAMf⟩⟩,
+    rcases hg with ⟨Mg, ⟨Ag, hAMg⟩⟩,
+    existsi [Mf + Mg, max Af Ag],
     intros z hz,
-    simp,
-    apply le_trans (complex.abs_add _ _),
-    apply add_le_add,
-    { refine hAMf z _,
-      exact le_trans (le_max_left _ _) hz },
-    { refine hAMg z _,
-      exact le_trans (le_max_right _ _) hz }
+    specialize hAMf z (le_trans (le_max_left Af Ag) hz),
+    specialize hAMg z (le_trans (le_max_right Af Ag) hz),
+
+    exact calc abs ((f + g) z) ≤ abs (f z) + abs (g z) : by mono
+    ... ≤ Mf + Mg : by mono
   end,
   smul  := λ c f hyp, begin
     cases hyp with M hM,
     cases hM with A hAM,
-    existsi (abs c * M),
-    existsi A,
+    existsi [abs c * M, A],
     intros z hz,
-    simp,
-    apply mul_le_mul_of_nonneg_left (hAM z hz) (complex.abs_nonneg c)
+    simpa using mul_le_mul_of_nonneg_left (hAM z hz) (complex.abs_nonneg c)
   end }
 
 instance : is_submodule is_zero_at_infinity :=
-{ zero_ := λ ε hε, ⟨1, λ z hz, by simp; exact le_of_lt hε⟩,
+{ zero_ := λ ε hε, ⟨1, λ z hz, by simp [le_of_lt hε]⟩,
   add_  := λ f g hf hg ε hε, begin
     cases hf (ε/2) (half_pos hε) with Af hAf,
     cases hg (ε/2) (half_pos hε) with Ag hAg,
     existsi (max Af Ag),
     intros z hz,
-    simp,
-    apply le_trans (complex.abs_add _ _),
+    specialize hAf z (le_trans (le_max_left _ _) hz),
+    specialize hAg z (le_trans (le_max_right _ _) hz),
     rw show ε = ε / 2 + ε / 2, by simp,
-    apply add_le_add,
-    { refine hAf z (le_trans (le_max_left _ _) hz) },
-    { refine hAg z (le_trans (le_max_right _ _) hz) }
+    exact le_trans (complex.abs_add _ _) (add_le_add hAf hAg)
   end,
   smul  := λ c f hyp ε hε, begin
     by_cases hc : (c = 0),
@@ -81,14 +75,9 @@ instance : is_submodule is_zero_at_infinity :=
     { cases hyp (ε / abs c) (lt_div_of_mul_lt (by simp [hc]) (by simp; exact hε)) with A hA,
       existsi A,
       intros z hz,
-      simp,
-      rw show ε = abs c * (ε / abs c),
-      begin
-        rw [mul_comm],
-        refine (div_mul_cancel _ _).symm,
-        simp [hc]
-      end,
-      apply mul_le_mul_of_nonneg_left (hA z hz) (complex.abs_nonneg c), }
+      rw show abs ((c • f) z) = (abs c)* abs (f z), by simp,
+      rw show ε = abs c * (ε / abs c), by rw [mul_comm] ; simp [hc, div_mul_cancel],
+      exact mul_le_mul_of_nonneg_left (hA z hz) (complex.abs_nonneg c) }
   end }
 
 lemma is_bound_at_infinity_of_is_zero_at_infinity {f : ℍ → ℂ} (h : is_zero_at_infinity f) :
@@ -114,4 +103,3 @@ instance (k : ℕ) : is_submodule (is_cusp_form k) := is_submodule.inter_submodu
 λ f,
 (m^k.pred : ℂ) • (sorry : modular_forms k) -- why is this • failing?
  -/
- 
