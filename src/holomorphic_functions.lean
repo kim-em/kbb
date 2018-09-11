b7866393cc049ab0f94dc44da39ae9b27193c9a9
kckennylau <kc_kennylau@yahoo.com.hk>
2018-09-11 16:26:49 +0800

    f,g holomorphic -> fg holomorphic

--- src/holomorphic_functions.lean
+++ src/holomorphic_functions.lean
@@ -18,14 +18,46 @@ let error_term (h : ℂ) : ℝ :=
     abs((f (z + h) - (f z + f'z * h))/h) in
 (tendsto error_term (nhds 0) (nhds 0))
 
+lemma has_complex_derivative_at_iff (f f'z z) :
+  has_complex_derivative_at f f'z z
+  ↔ (∀ ε > 0, ∃ δ > 0, ∀ h:ℂ, h ≠ 0 → abs h < δ → abs ((f(z+h)-f(z)-f'z*h)/h) < ε) :=
+⟨λ H ε hε, let ⟨δ, hδ1, hδ2⟩ := tendsto_nhds_of_metric.1 H ε hε in
+  ⟨δ, hδ1, λ h h1 h2, by simp only [dist, sub_zero, complex.abs_abs, sub_add_eq_sub_sub] at hδ2;
+    from hδ2 h2⟩,
+λ H, tendsto_nhds_of_metric.2 $ λ ε hε, let ⟨δ, hδ1, hδ2⟩ := H ε hε in
+  ⟨δ, hδ1, λ h h1, if H : h = 0 then by unfold dist;
+    rwa [H, add_zero, mul_zero, add_zero, sub_self, zero_div, sub_zero, complex.abs_zero, _root_.abs_zero]
+  else by unfold dist at h1 ⊢; rw [sub_zero] at h1;
+    rw [sub_zero, complex.abs_abs, sub_add_eq_sub_sub]; from hδ2 h H h1⟩⟩
+
+lemma has_complex_derivative_at_iff' (f f'z z) :
+  has_complex_derivative_at f f'z z
+  ↔ (∀ ε > 0, ∃ δ > 0, ∀ h:ℂ, abs h < δ → abs ((f(z+h)-f(z)-f'z*h)/h) < ε) :=
+by simp only [has_complex_derivative_at, tendsto_nhds_of_metric, dist,
+sub_zero, complex.abs_abs, sub_add_eq_sub_sub]
+
+lemma has_complex_derivative_at_iff'' (f f'z z) :
+  has_complex_derivative_at f f'z z
+  ↔ tendsto (λ h, (f(z+h)-f(z)-f'z*h)/h) (nhds 0) (nhds 0) :=
+by simp only [has_complex_derivative_at, tendsto_nhds_of_metric, dist,
+sub_zero, complex.abs_abs, sub_add_eq_sub_sub]
+
 section
-variables  {α : Type*} {β : Type*} {s : set α}
+variables {α : Type*} {β : Type*} {s : set α}
 
 -- I would like to remove the following line... but I can't
 instance foobar (X : Type u) (R : Type v) [ring R] : module R (X → R) := pi.module
 
 def extend_by_zero [has_zero β] (f : s → β) : α → β :=
 λ z, if h : z ∈ s then f ⟨z, h⟩ else 0
+
+lemma extend_by_zero_zero [has_zero β] :
+extend_by_zero (λ s, 0 : s → β) = (λ h, 0) :=
+by ext z; by_cases h : z ∈ s; simp [extend_by_zero, h]
+
+lemma extend_by_zero_zero' [has_zero β] :
+extend_by_zero (0 : s → β) = 0 :=
+by ext z; by_cases h : z ∈ s; simp [extend_by_zero, h]
 
 lemma extend_by_zero_add [add_group β] (f g : s → β) :
 extend_by_zero (f + g) = extend_by_zero f + extend_by_zero g :=
@@ -39,6 +71,10 @@ lemma extend_by_zero_neg [add_group β] (f : s → β) :
 extend_by_zero (-f) = -extend_by_zero f :=
 by ext z; by_cases h : z ∈ s; simp [extend_by_zero, h]
 
+lemma extend_by_zero_sub [add_group β] (f g : s → β) :
+extend_by_zero (f - g) = extend_by_zero f - extend_by_zero g :=
+(extend_by_zero_add _ _).trans $ congr_arg _ $ extend_by_zero_neg g
+
 lemma extend_by_zero_smul [ring β] (c : β) (f : s → β) :
 extend_by_zero (c • f) = c • extend_by_zero f :=
 by ext z; by_cases h : z ∈ s; simp [extend_by_zero, h]
@@ -46,46 +82,21 @@ by ext z; by_cases h : z ∈ s; simp [extend_by_zero, h]
 end
 
 /-- holomorphic function from a subset of ℂ. Correct definition if domain is open. -/
-def is_holomorphic {domain : set ℂ} (f : domain → ℂ) :=
-(∀ z : domain, ∃ f'z, has_complex_derivative_at (extend_by_zero f) f'z z)
+def is_holomorphic {domain : set ℂ} (f : domain → ℂ) : Prop :=
+∀ z : domain, ∃ f'z, has_complex_derivative_at (extend_by_zero f) f'z z
 
 variable {domain : set ℂ}
-/-
-instance : is_subring {f : domain → ℂ | is_holomorphic f} :=
-{ zero_mem := sorry,
-  add_mem  := sorry,
-  neg_mem  := sorry,
-  mul_mem  := sorry,
-  one_mem  := sorry,
-}-/
 
 lemma const_hol (domain_open : is_open domain) (c : ℂ) : is_holomorphic (λ z : domain, (c : ℂ)) :=
-λ z₀, ⟨(0 : ℂ), tendsto_nhds (λ S h0 hS, begin
-  rw nhds_sets,
-  existsi {h : ℂ | ↑z₀ + h ∈ domain},
-  split,
-  { intros h H,
-    dsimp [extend_by_zero],
-    rwa show abs
-      ((dite (↑z₀ + h ∈ domain) (λ (h : ↑z₀ + h ∈ domain), c) (λ (h : ↑z₀ + h ∉ domain), 0) +
-            -(dite (↑z₀ ∈ domain) (λ (h : ↑z₀ ∈ domain), c) (λ (h : ↑z₀ ∉ domain), 0) + 0 * h)) /
-         h) = 0,
-    by erw [complex.abs_eq_zero, dif_pos H, dif_pos z₀.property]; ring },
-  { exact ⟨continuous_add continuous_const continuous_id domain domain_open,
-    by simpa using z₀.property⟩ }
-end)⟩
+λ z₀, ⟨(0 : ℂ), let ⟨δ, hδ1, hδ2⟩ := is_open_metric.1 domain_open z₀.1 z₀.2 in
+tendsto_nhds_of_metric.2 $ λ ε hε, ⟨δ, hδ1, λ z hz,
+have H1 : ↑z₀ + z ∈ domain, from show z₀.1 + z ∈ domain,
+  from hδ2 $ by simpa [dist] using hz,
+have H2 : ↑z₀ ∈ domain, from z₀.2,
+by simpa [extend_by_zero, H1, H2, -add_comm] using hε⟩⟩
 
 lemma zero_hol : is_holomorphic (λ z : domain, (0 : ℂ)) :=
-begin
-  intro z₀,
-  existsi (0 : ℂ),
-  dsimp [has_complex_derivative_at],
-  have : extend_by_zero (λ (z : {x // x ∈ domain}), (0:ℂ)) = (λ (z : ℂ), 0),
-  { ext z,
-    dsimp only [extend_by_zero],
-    by_cases h : z ∈ domain ; simp [h] ; refl },
-  simp [this, tendsto_const_nhds]
-end
+λ z₀, ⟨0, show tendsto _ _ _, by simp [extend_by_zero_zero, tendsto_const_nhds]⟩
 
 lemma one_hol (domain_open : is_open domain) : is_holomorphic (λ z : domain, (1 : ℂ)) := const_hol domain_open 1
 
@@ -96,16 +107,11 @@ begin
   cases g_hol z₀ with g'z₀ Hg,
   existsi (f'z₀ + g'z₀),
   rw extend_by_zero_add,
-  have Hfg : tendsto (λ (h : ℂ), abs ((extend_by_zero f (↑z₀ + h) - (extend_by_zero f ↑z₀ + f'z₀ * h)) / h) +
-         abs ((extend_by_zero g (↑z₀ + h) - (extend_by_zero g ↑z₀ + g'z₀ * h)) / h)) (nhds 0) (nhds 0) :=
-  by simpa using tendsto_add Hf Hg,
-  refine squeeze_zero _ _ Hfg,
-  { intro h, apply complex.abs_nonneg },
-  { intro h,
-    refine le_trans (le_of_eq _) (complex.abs_add _ _),
-    congr,
-    dsimp,
-    ring }
+  rw has_complex_derivative_at_iff'' at *,
+  convert tendsto_add Hf Hg,
+  ext h, rw ← add_div,
+  change ((_+_)-(_+_)-_*_)/_=_,
+  simp only [add_mul, sub_eq_add_neg, neg_add, add_assoc, add_left_comm]
 end
 
 lemma mul_hol (f g : domain → ℂ) (f_hol : is_holomorphic f) (g_hol : is_holomorphic g) : is_holomorphic (f * g) :=
@@ -113,27 +119,31 @@ begin
   intro z₀,
   cases f_hol z₀ with f'z₀ Hf,
   cases g_hol z₀ with g'z₀ Hg,
-  existsi f'z₀*(g z₀) + (f z₀)*g'z₀,
-  let F := extend_by_zero f,
-  let G := extend_by_zero g,
+  existsi f'z₀*(extend_by_zero g z₀) + (extend_by_zero f z₀)*g'z₀,
   rw extend_by_zero_mul,
-  let ε := λ h, F (z₀ + h) - f z₀ - f'z₀*h,
-  let η := λ h, G (z₀ + h) - g z₀ - g'z₀*h,
-  have : ∀ h, (F * G) (z₀ + h) = (f z₀) * (g z₀) + (f'z₀*(g z₀) + (f z₀)*g'z₀)*h +
-  (g z₀ + g'z₀*h + η h)*(ε h) + (f z₀ + f'z₀*h)*(η h) + f'z₀*g'z₀*h*h,
-  by intros h ; dsimp[ε, η] ; ring,
-
-  change tendsto
-    (λ (h : ℂ),
-       abs
-         (((F*G) (z₀ + h) - (F z₀ * G z₀ + (f'z₀ * g z₀ + f z₀ * g'z₀) * h)) /
-            h)) (nhds 0) (nhds 0),
-  have H1 : F z₀ = f z₀, by cases z₀ with v p ; simp [F, p, extend_by_zero],
-  have H2 : G z₀ = g z₀, by cases z₀ with v p ; simp [G, p, extend_by_zero],
-  simp only [this, H1, H2],
-  ring,
-
-  sorry
+  rw has_complex_derivative_at_iff'' at *,
+  generalize_hyp : extend_by_zero f = F at Hf ⊢,
+  generalize_hyp : extend_by_zero g = G at Hg ⊢,
+  have H1 : (λ h, ((F*G) (↑z₀+h) - (F*G) ↑z₀ - (f'z₀ * G z₀ + F z₀ * g'z₀)*h)/h)
+    = (λ h, ((F(↑z₀+h)-F(↑z₀)-f'z₀*h)/h)*((G(↑z₀+h)-G(↑z₀)-g'z₀*h)/h)*h
+      + f'z₀*g'z₀*h
+      + ((F(↑z₀+h)-F(↑z₀)-f'z₀*h)/h)*(g'z₀*h+G(↑z₀))
+      + (F(↑z₀)+f'z₀*h)*((G(↑z₀+h)-G(↑z₀)-g'z₀*h)/h)),
+  { funext h, change (_*_-_*_-_)/_=_,
+    by_cases h0 : h = 0,
+    { subst h0, simp },
+    apply (div_eq_iff_mul_eq h0).2,
+    simp only [add_div, sub_div, add_mul, sub_mul, mul_add, mul_sub],
+    simp only [div_mul_div, div_mul_eq_mul_div, (mul_div_assoc _ _ _).symm],
+    simp only [mul_div_cancel _ h0], ring },
+  have H2 : (0:ℂ) = 0*0*0+f'z₀*g'z₀*0+0*(g'z₀*0+G(↑z₀))+(F(↑z₀)+f'z₀*0)*0,
+  { simp only [zero_mul, mul_zero, zero_add] },
+  conv { congr, rw H1, skip, skip, rw H2 },
+  exact tendsto_add (tendsto_add (tendsto_add
+    (tendsto_mul (tendsto_mul Hf Hg) tendsto_id)
+    (tendsto_mul tendsto_const_nhds tendsto_id))
+    (tendsto_mul Hf (tendsto_add (tendsto_mul tendsto_const_nhds tendsto_id) tendsto_const_nhds)))
+    (tendsto_mul (tendsto_add tendsto_const_nhds (tendsto_mul tendsto_const_nhds tendsto_id)) Hg)
 end
 
 lemma neg_hol (f : domain → ℂ) (f_hol : is_holomorphic f) : is_holomorphic (-f) :=
@@ -148,6 +158,14 @@ begin
   conv { congr, funext, rw [neg_div, complex.abs_neg], },
   exact H
 end
+
+instance (domain_open : is_open domain) : is_subring {f : domain → ℂ | is_holomorphic f} :=
+{ zero_mem := zero_hol,
+  add_mem  := add_hol,
+  neg_mem  := neg_hol,
+  mul_mem  := mul_hol,
+  one_mem  := one_hol domain_open,
+}
 
 instance xyzzy {F : Type u} [normed_field F] : normed_space F F :=
 { dist_eq := normed_field.dist_eq,
