c625649d8bb47f70977f2d554acc20011f7bdbc0
Reid Barton <rwbarton@gmail.com>
2018-09-11 08:10:41 -0400

    Pattern matching on fast_matrix

--- src/fast_matrices.lean
+++ src/fast_matrices.lean
@@ -1,8 +1,6 @@
-import tactic.tidy
 import data.vector
-import .matrices
 
-def vector.mk {α : Type*} {n : ℕ} (l : list α) (pr : l.length = n) :
+@[pattern] def vector.mk {α : Type*} {n : ℕ} (l : list α) (pr : l.length = n) :
   vector α n := ⟨l, pr⟩
 
 notation `![` l:(foldr `, ` (h t, list.cons h t) list.nil `]`) :=
@@ -13,3 +11,7 @@ def {u} fast_matrix (m n : ℕ) (α : Type u) : Type u := vector (vector α n) m
 example : fast_matrix 2 3 ℤ := 
 ![![ 1 , 1,  5 ],
   ![ 0 , 1, -2 ]]
+
+example : fast_matrix 2 2 ℤ → ℤ |
+![![a,b],
+  ![c,d]] := a + b + c + d
