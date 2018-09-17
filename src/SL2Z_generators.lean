77a5dffa62a617023ce765f832d3517912e899d0
Johannes Hölzl <johannes.hoelzl@gmx.de>
2018-09-17 19:04:24 +0200

    cleanup SL2Z_generators

--- src/SL2Z_generators.lean
+++ src/SL2Z_generators.lean
@@ -10,6 +10,13 @@ theorem int.mul_eq_one {m n : ℤ} :
     or.inr ⟨H2, by rwa [H2, neg_one_mul, neg_eq_iff_neg_eq, eq_comm] at H⟩),
 by simp [or_imp_distrib] {contextual := tt}⟩
 
+namespace int
+
+lemma nat_abs_lt_nat_abs (i k : ℤ) (hi : 0 ≤ i) (h : i < abs k) : nat_abs i < nat_abs k :=
+coe_nat_lt.1 $ by rwa [nat_abs_of_nonneg hi, ← int.abs_eq_nat_abs]
+
+end int
+
 namespace SL2Z_M
 
 def S : SL2Z := ⟨0, -1, 1, 0, rfl⟩
@@ -66,159 +73,113 @@ end
 
 def reduce_step (A : Mat m) : Mat m := SL2Z_M m S (SL2Z_M m (T^(-(A.a/A.c))) A)
 
+@[elab_as_eliminator]
+def reduce_rec (m : ℤ) {C : Mat m → Sort*}
+  (h₀ : ∀A:Mat m, A.c = 0 → C A)
+  (h₁ : ∀A:Mat m, int.nat_abs A.c ≠ 0 → C (reduce_step A) → C A) :
+  ∀A, C A | A :=
+if hc : int.nat_abs A.c = 0 then h₀ A (int.eq_zero_of_nat_abs_eq_zero hc)
+else
+  have int.nat_abs (reduce_step A).c < int.nat_abs A.c, from reduce_aux m A hc,
+  h₁ A hc (reduce_rec (reduce_step A))
+using_well_founded
+  { rel_tac := λ _ _, `[exact ⟨_, measure_wf (int.nat_abs ∘ integral_matrices_with_determinant.c)⟩]}
+
 /-- correct if m ≠ 0 -/
 def reduce (m : ℤ) : Mat m → Mat m | A :=
-if H1 : int.nat_abs A.c = 0
-then if H2 : A.a > 0
+if hc : int.nat_abs A.c = 0
+then if ha : 0 < A.a
   then SL2Z_M m (T^(-(A.b/A.d))) A
   else SL2Z_M m (T^(-(-A.b/ -A.d))) (SL2Z_M m S (SL2Z_M m S A))
 else
-  have int.nat_abs (reduce_step A).c < int.nat_abs A.c, from reduce_aux m A H1,
+  have int.nat_abs (reduce_step A).c < int.nat_abs A.c, from reduce_aux m A hc,
   reduce (reduce_step A)
 using_well_founded
   { rel_tac := λ _ _, `[exact ⟨_, measure_wf (int.nat_abs ∘ integral_matrices_with_determinant.c)⟩]}
 
-theorem reduce_def (m : ℤ) (A : Mat m) : reduce m A =
-  if int.nat_abs A.c = 0
-  then if H2 : A.a > 0
-    then SL2Z_M m (T^(-(A.b/A.d))) A
-    else SL2Z_M m (T^(-(-A.b/ -A.d))) (SL2Z_M m S (SL2Z_M m S A))
-  else reduce m (SL2Z_M m S (SL2Z_M m (T^(-(A.a/A.c))) A)) :=
-SL2Z_M.reduce.equations._eqn_1 _ _
+@[simp] theorem reduce_eq1 (m : ℤ) {A : Mat m} (hc : int.nat_abs A.c = 0) (ha : 0 < A.a) :
+  reduce m A = SL2Z_M m (T^(-(A.b/A.d))) A :=
+by rw [reduce.equations._eqn_1 _ _, if_pos hc, if_pos ha]; refl
 
-theorem reduce_eq_of_zero (m : ℤ) {A : Mat m} (h : int.nat_abs A.c = 0) :
-  reduce m A = (if H2 : A.a > 0
-    then SL2Z_M m (T^(-(A.b/A.d))) A
-    else SL2Z_M m (T^(-(-A.b/ -A.d))) (SL2Z_M m S (SL2Z_M m S A))) :=
-by rw [reduce_def, if_pos h]
+@[simp] theorem reduce_eq2 (m : ℤ) {A : Mat m} (hc : int.nat_abs A.c = 0) (ha : ¬ 0 < A.a) :
+  reduce m A = SL2Z_M m (T^(-(-A.b/ -A.d))) (SL2Z_M m S (SL2Z_M m S A)) :=
+by rw [reduce.equations._eqn_1 _ _, if_pos hc, if_neg ha]; refl
 
-theorem reduce_eq (m : ℤ) {A : Mat m} (h : int.nat_abs A.c ≠ 0) :
+@[simp] theorem reduce_eq3 (m : ℤ) {A : Mat m} (h : int.nat_abs A.c ≠ 0) :
   reduce m A = reduce m (reduce_step A) :=
-by rw [reduce_def, reduce_step, if_neg h]
+by rw [reduce.equations._eqn_1 _ _, if_neg h]
 
-theorem reduce_mem_reps (m : ℤ) (Hm : m ≠ 0) (A : Mat m) : reduce m A ∈ reps m :=
+theorem reduce_spec (m : ℤ) : ∀A : Mat m, ∃ S, SL2Z_M m S A = reduce m A :=
 begin
-  suffices : ∀ n (A : Mat m), n = int.nat_abs A.c →
-    (reduce m A).c = 0 ∧
-    0 < (reduce m A).a ∧
-    0 ≤ (reduce m A).b ∧
-    int.nat_abs (reduce m A).b < int.nat_abs (reduce m A).d,
-    from this _ A rfl,
-  intro n, clear A,
-  apply nat.strong_induction_on n,
-  intros n ih A H, subst H,
-  by_cases h1 : int.nat_abs (A.c) = 0,
-  { rw [reduce_def, if_pos h1],
-    by_cases h2 : A.a > 0,
-    { rw [dif_pos h2],
-      have h3 : A.c = 0 := int.eq_zero_of_nat_abs_eq_zero h1,
-      simp only [h3, zero_mul, one_mul, mul_zero, add_zero, zero_add] with SL2Z,
-      rw [← neg_mul_eq_neg_mul, ← sub_eq_add_neg, mul_comm, ← int.mod_def],
-      have h4 : A.d ≠ 0,
-      { intro h4, apply Hm, rw [← A.det, h3, h4, mul_zero, mul_zero, sub_zero] },
-      refine ⟨rfl, h2, int.mod_nonneg _ h4, _⟩,
-      rw [← int.coe_nat_lt, int.nat_abs_of_nonneg (int.mod_nonneg _ h4)],
-      rw [← int.abs_eq_nat_abs],
-      exact int.mod_lt _ h4 },
-    { rw [dif_neg h2],
-      have h3 : A.c = 0 := int.eq_zero_of_nat_abs_eq_zero h1,
-      have h4 : -A.d ≠ 0,
-      { intro h4, rw [neg_eq_zero] at h4, apply Hm,
-        rw [← A.det, h3, h4, mul_zero, mul_zero, sub_zero] },
-      have h5 : A.a ≠ 0,
-      { intro h5, apply Hm, rw [← A.det, h3, h5, zero_mul, mul_zero, sub_zero] },
-      have h6 : -A.a > 0,
-      { exact neg_pos_of_neg ((lt_or_eq_of_le (le_of_not_lt h2)).resolve_right h5) },
-      simp only [zero_mul, neg_zero, one_mul, mul_zero, h3, add_zero, zero_add, neg_one_mul, mul_neg_one] with SL2Z,
-      rw [← neg_mul_eq_neg_mul, ← sub_eq_add_neg, mul_comm, ← int.mod_def],
-      refine ⟨rfl, h6, int.mod_nonneg _ h4, _⟩,
-      rw [← int.coe_nat_lt, int.nat_abs_of_nonneg (int.mod_nonneg _ h4)],
-      rw [← int.abs_eq_nat_abs],
-      exact int.mod_lt _ h4 } },
-  { rw [reduce_def, if_neg h1],
-    exact ih _ (reduce_aux _ _ h1) _ rfl }
+  refine reduce_rec m _ _,
+  { intros A hc,
+    by_cases ha : 0 < A.a,
+    { simp [*], exact ⟨_, rfl⟩ },
+    { simp [*, (is_monoid_action.mul (SL2Z_M m) _ _ _).symm], exact ⟨_, rfl⟩ } },
+  { rintros A hc ⟨S, eq⟩,
+    rw [reduce_eq3 m hc, ← eq, reduce_step, ← is_monoid_action.mul (SL2Z_M m), ← is_monoid_action.mul (SL2Z_M m)],
+    exact ⟨_, rfl⟩ }
+end
+
+theorem reduce_mem_reps (m : ℤ) (hm : m ≠ 0) : ∀(A : Mat m), reduce m A ∈ reps m :=
+begin
+  refine reduce_rec m (assume A (c_eq : A.c = 0), _) (assume A hc ih, _),
+  { have hd : A.d ≠ 0, { intro d_eq, apply hm, rw [← A.det, d_eq, c_eq, mul_zero, mul_zero, sub_zero] },
+    have eq : ∀b d, b + -(b / d * d) = b % d, { intros, rw [← sub_eq_add_neg, mul_comm, ← int.mod_def] },
+    have h : ∀(a b : ℤ), 0 < a → 0 < a ∧ 0 ≤ b % A.d ∧ int.nat_abs (b % A.d) < int.nat_abs A.d :=
+      assume a b ha, ⟨ha, int.mod_nonneg _ hd, int.nat_abs_lt_nat_abs _ _ (int.mod_nonneg _ hd) (int.mod_lt _ hd)⟩,
+    by_cases ha : 0 < A.a,
+    { simpa [reduce_eq1, reps, c_eq, ha, eq] using h _ _ ha },
+    { have a_ne : A.a ≠ 0,
+      { intro a_eq, apply hm, rw [← A.det, a_eq, c_eq, zero_mul, mul_zero, sub_zero] },
+      have a_pos : -A.a > 0 := neg_pos_of_neg (lt_of_le_of_ne (le_of_not_gt ha) a_ne),
+      simpa [reduce_eq2, reps, c_eq, ha, eq] using h _ _ a_pos } },
+  { rwa [reduce_eq3 m hc] }
 end
 
 @[elab_as_eliminator]
-protected theorem induction_on {C : (Mat m) → Prop} (A : Mat m) (mne0 : m ≠ 0)
-  (H0 : ∀ {A : Mat m} (h0 : A.c = 0) (h1 : A.a * A.d = m) (h2 : 0 < A.a) (h3 : 0 ≤ A.b) (h4 : int.nat_abs A.b < int.nat_abs A.d), C A)
-  (HS : ∀ B, C B → C (SL2Z_M m S B)) (HT : ∀ B, C B → C (SL2Z_M m T B)) : C A :=
-have hSid : ∀ B, (SL2Z_M m S (SL2Z_M m S (SL2Z_M m S (SL2Z_M m S B)))) = B, from λ B, by ext; simp,
-have HS' : ∀ B, C (SL2Z_M m S B) → C B,
-  from λ B ih, have H : _ := (HS _ $ HS _ $ HS _ ih), by rwa hSid B at H,
-have hTinv : ∀ B, SL2Z_M m S (SL2Z_M m S (SL2Z_M m S (SL2Z_M m T (SL2Z_M m S (SL2Z_M m T (SL2Z_M m S B)))))) = SL2Z_M m T⁻¹ B,
-  from λ B, by repeat {rw [←is_monoid_action.mul (SL2Z_M m)]}; congr,
-have HT' : ∀ B, C B → C (SL2Z_M m T⁻¹ B),
-  from λ B ih, by {have H := (HS _ $ HS _ $ HS _ $ HT _ $ HS _ $ HT _ $ HS _ ih), by rwa [hTinv] at H},
-have HT3 : ∀ B, C (SL2Z_M m T B) → C B, from λ B ih,
-  begin
-    have H := HT' (SL2Z_M m T B) ih,
-    rw [←is_monoid_action.mul (SL2Z_M m)] at H,
-    rw mul_left_inv at H,
-    rw [is_monoid_action.one (SL2Z_M m)] at H,
-    exact H
-  end,
-have HT4 : ∀ B, C (SL2Z_M m T⁻¹ B) → C B, from λ B ih,
-  begin
-    have H := HT (SL2Z_M m T⁻¹ B) ih,
-    rw [←is_monoid_action.mul (SL2Z_M m)] at H,
-    simp at H,
-    rw [is_monoid_action.one (SL2Z_M m)] at H,
-    exact H
-  end,
-have HT5 : ∀ B (n:ℤ), C (SL2Z_M m (T^n) B) → C B, from λ B n,
-  int.induction_on n (by rw [gpow_zero, is_monoid_action.one (SL2Z_M m)]; from id)
-    (λ i ih1 ih2, ih1 $ HT3 _ $ begin
-      conv { congr, rw ←is_monoid_action.mul (SL2Z_M m) },
-      conv at ih2 { congr, rw [add_comm, gpow_add, gpow_one] },
-      assumption end)
-    (λ i ih1 ih2, ih1 $ HT4 _ $ begin
-      conv { congr, rw ←is_monoid_action.mul (SL2Z_M m) },
-      conv at ih2 { congr, rw [sub_eq_neg_add, gpow_add, gpow_neg_one] },
-      assumption end),
-have Hred : C (reduce m A),
+protected theorem induction_on {C : Mat m → Prop} (A : Mat m) (mne0 : m ≠ 0)
+  (h0 : ∀{A:Mat m}, A.c = 0 → A.a * A.d = m → 0 < A.a → 0 ≤ A.b → int.nat_abs A.b < int.nat_abs A.d → C A)
+  (hS : ∀ B, C B → C (SL2Z_M m S B)) (hT : ∀ B, C B → C (SL2Z_M m T B)) :
+  C A :=
+have S_eq : ∀ B, (SL2Z_M m S (SL2Z_M m S (SL2Z_M m S (SL2Z_M m S B)))) = B,
+  by intro b; ext; simp,
+have hS' : ∀{B}, C (SL2Z_M m S B) → C B, from
+  λ B ih, have h : _ := (hS _ $ hS _ $ hS _ ih), by rwa S_eq B at h,
+have eq : ∀ B,
+  (SL2Z_M m S $ SL2Z_M m S $ SL2Z_M m S $ SL2Z_M m T $ SL2Z_M m S $ SL2Z_M m T $ SL2Z_M m S B) =
+    SL2Z_M m T⁻¹ B,
+  by intro b; repeat {rw [←is_monoid_action.mul (SL2Z_M m)]}; congr,
+have hT_inv : ∀ B, C B → C (SL2Z_M m T⁻¹ B), from
+  λ B ih, have h : _ := (hS _ $ hS _ $ hS _ $ hT _ $ hS _ $ hT _ $ hS _ ih), by rwa eq at h,
+have hT' : ∀B, C (SL2Z_M m T B) → C B,
+{ assume B ih,
+  have h := hT_inv (SL2Z_M m T B) ih,
+  rwa [←is_monoid_action.mul (SL2Z_M m), mul_left_inv, is_monoid_action.one (SL2Z_M m)] at h },
+have hT_inv' : ∀ B, C (SL2Z_M m T⁻¹ B) → C B,
+{ assume B ih,
+  have H := hT (SL2Z_M m T⁻¹ B) ih,
+  rwa [←is_monoid_action.mul (SL2Z_M m), mul_right_inv, is_monoid_action.one (SL2Z_M m)] at H },
+have hTpow' : ∀{B} {n:ℤ}, C (SL2Z_M m (T^n) B) → C B,
+{ refine assume B n, int.induction_on n (λh, _) (λi ih1 ih2, ih1 $ hT' _ _) (λi ih1 ih2, ih1 $ hT_inv' _ _),
+  { rwa [gpow_zero, is_monoid_action.one (SL2Z_M m)] at h },
+  { rwa [add_comm, gpow_add, gpow_one, is_monoid_action.mul (SL2Z_M m)] at ih2 },
+  { rwa [sub_eq_neg_add, gpow_add, gpow_neg_one, is_monoid_action.mul (SL2Z_M m)] at ih2 } },
+have h_reduce : C (reduce m A),
 { rcases reduce_mem_reps m mne0 A with ⟨H1, H2, H3, H4⟩,
-  refine H0 H1 _ H2 H3 H4,
+  refine h0 H1 _ H2 H3 H4,
   simpa only [H1, mul_zero, sub_zero] using (reduce m A).det },
-suffices ∀ n (A : Mat m), int.nat_abs A.c = n → C (reduce m A) → C A,
-  from this _ _ rfl Hred,
-λ n_stupid, nat.strong_induction_on n_stupid $ λ n ih A H1 H2,
+suffices ∀A : Mat m, C (reduce m A) → C A, from this _ h_reduce,
 begin
-  subst H1,
-  rw reduce_def at H2, split_ifs at H2 with H3 H4,
-  { exact HT5 _ _ H2 },
-  { exact HS' _ (HS' _ (HT5 _ _ H2)) },
-  have H4 := ih _ (reduce_aux _ _ H3) _ rfl H2,
-  exact HT5 _ _ (HS' _ H4)
+  refine reduce_rec m _ _,
+  { assume A c_eq ih,
+    by_cases ha : 0 < A.a,
+    { simp [reduce_eq1, c_eq, ha, -gpow_neg] at ih, exact hTpow' ih },
+    { simp [reduce_eq2, c_eq, ha] at ih, exact (hS' $ hS' $ hTpow' ih) } },
+  { assume A hc ih hA,
+    rw [reduce_eq3 m hc] at hA,
+    exact (hTpow' $ hS' $ ih hA) }
 end
-
--- theorem closure : group.closure ({S, T} : set SL2Z) = set.univ :=
--- set.eq_univ_of_forall $ λ A, SL2Z_M.induction_on A
---   (group.in_closure.one _)
---   (λ B ih, group.in_closure.mul (group.in_closure.basic $ or.inr $ or.inl rfl) ih)
---   (λ B ih, group.in_closure.mul (group.in_closure.basic $ or.inl rfl) ih)
-
--- (h0 : A.c = 0) (h1 : A.a * A.d = m) (h2 : 0 ≤ A.a) (h3 : 0 ≤ A.b)
--- (h4 : int.nat_abs A.b ≤ int.nat_abs A.d)
-
-theorem reduce_spec (m : ℤ) : ∀A:Mat m, ∃ S, SL2Z_M m S A = reduce m A | A :=
-if h : int.nat_abs A.c = 0 then
-begin
-  rw [reduce_eq_of_zero m h],
-  split_ifs,
-  { exact ⟨_, rfl⟩ },
-  { rw [← is_monoid_action.mul (SL2Z_M m), ← is_monoid_action.mul (SL2Z_M m)],
-    exact ⟨_, rfl⟩ }
-end
-else
-  have int.nat_abs (reduce_step A).c < int.nat_abs A.c, from reduce_aux m A h,
-  let ⟨S, eq⟩ := reduce_spec (reduce_step A) in
-  begin
-    rw [reduce_eq m h, ← eq, reduce_step, ← is_monoid_action.mul (SL2Z_M m), ← is_monoid_action.mul (SL2Z_M m)],
-    exact ⟨_, rfl⟩
-  end
-using_well_founded
-  { rel_tac := λ _ _, `[exact ⟨_, measure_wf (int.nat_abs ∘ integral_matrices_with_determinant.c)⟩]}
 
 theorem reps_unique (m : ℤ) (hm : m ≠ 0) :
   ∀(M : SL2Z) (A B : Mat m), A ∈ reps m → B ∈ reps m → SL2Z_M m M A = B → A = B :=
